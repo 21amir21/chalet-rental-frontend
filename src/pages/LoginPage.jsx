2a9a74339034954ecc66332b30d0a098f81c6e38
Amir Osama <amirosama21@gmail.com>
2024-12-01 19:01:22 +0200

    Implement login functionality with form handling and user authentication

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import { setLogin } from "../redux/state";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const loggedIn = await response.json();
+      if (loggedIn) {
+        dispatch(
+          setLogin({
+            user: loggedIn.user, // TODO: look if u are gonna make it return the whole user or the user._id
+            token: loggedIn.jwt, // TODO: will see if it should be `token` not `jwt`
+          })
+        );
+        navigate("/");
+      }
+    } catch (err) {
+      console.error(`Error while logging in: ${err.message}`);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <form className="login_content_form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">LOG IN</button>
+          <a href="/register">Dont have an account? Sign in here</a>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
