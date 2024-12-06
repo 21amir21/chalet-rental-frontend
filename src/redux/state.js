adf44b95ad15914907f1f307c24f75a6c7fe335d
Amir Osama <amirosama21@gmail.com>
2024-12-06 23:05:48 +0200

    Add actions to manage trip, wish, property, and reservation lists in userSlice

--- src/redux/state.js
+++ src/redux/state.js
@@ -20,8 +20,28 @@ export const userSlice = createSlice({
     setChalets: (state, action) => {
       state.chalets = action.payload.chalets;
     },
+    setTripList: (state, action) => {
+      state.user.tripList = action.payload;
+    },
+    setWishList: (state, action) => {
+      state.user.wishList = action.payload;
+    },
+    setPropertyList: (state, action) => {
+      state.user.propertyList = action.payload;
+    },
+    setReservationList: (state, action) => {
+      state.user.reservationList = action.payload;
+    },
   },
 });
 
-export const { setLogin, setLogOut, setChalets } = userSlice.actions;
+export const {
+  setLogin,
+  setLogOut,
+  setChalets,
+  setTripList,
+  setWishList,
+  setPropertyList,
+  setReservationList,
+} = userSlice.actions;
 export default userSlice.reducer;
