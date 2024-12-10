a562a6b1c4236a681f67d6eb36d642e3c4369c52
Amir Osama <amirosama21@gmail.com>
2024-12-10 16:01:27 +0200

    Refactor UserProfile component to include Footer, streamline form structure, and remove commented code

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../redux/state";
 
@@ -67,17 +68,6 @@ const UserProfile = ({ user }) => {
       if (response.ok) {
         const updatedUser = await response.json();
         dispatch(setUser({ user: updatedUser }));
-        // navigate(`/users/${user._id}`);
-        // window.location.reload();
-        // setFormData({
-        //   firstName: updatedUser.firstName,
-        //   lastName: updatedUser.lastName,
-        //   email: updatedUser.email,
-        //   password: updatedUser.password,
-        //   confirmPassword: updatedUser.confirmPassword,
-        //   userType: updatedUser.userType,
-        //   profileImage: updatedUser.profileImage,
-        // });
       }
     } catch (err) {
       console.error(`Update Failed: ${err.message}`);
@@ -89,96 +79,103 @@ const UserProfile = ({ user }) => {
   return (
     <>
       <NavBar />
-      <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          User Profile Management
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              First Name:
-            </label>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              Last Name:
-            </label>
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              Password:
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              Confirm Password:
-            </label>
-            <input
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-gray-600 font-medium mb-1">
-              Upload Photo:
-            </label>
-            <input
-              type="file"
-              name="profileImage"
-              onChange={handlePhotoChange}
-              accept="image/*"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 transition duration-200"
-          >
-            Save Changes
-          </button>
-        </form>
+      <div
+        className="mt-9 mb-9"
+        // TODO: ehh ra2yko ya shbab
+        // style={{ backgroundImage: 'url("/src/assets/welcome.jpg")' }}
+      >
+        <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
+          <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+            User Profile Management
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                First Name:
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                Last Name:
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                Confirm Password:
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-600 font-medium mb-1">
+                Upload Photo:
+              </label>
+              <input
+                type="file"
+                name="profileImage"
+                onChange={handlePhotoChange}
+                accept="image/*"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 transition duration-200"
+            >
+              Save Changes
+            </button>
+          </form>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
